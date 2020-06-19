6b5a52a81b20e63a6705a0a08c9550b1f15a2802
Rohmad <rohmad37@ymail.com>
2020-06-19 16:17:48 +0700

    Exercise Jumat 5

---
+++ script.js
@@ -0,0 +1,11 @@
+function transformText(str) {
+  let regexTest = /[A-Z]/g;
+
+  if (str.match(regexTest)) {
+    return str.toLowerCase();
+  } else {
+    return str.toUpperCase();
+  }
+}
+
+export default transformText;
